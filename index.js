495693b3864f4d369ea57e1918363eefacc404cf
Elnathan Mburu <elnathanmburu@gmail.com>
2023-10-02 23:14:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+const multiply= num1*num2;
+console.log(multiply);
+
+
+
+Math.random() * max | 0;
+
+const num3=16,num4=4;
+const mod=num3%num4;
+
+const max= Math.max(5,9,3,17,20);
+console.log(max);
